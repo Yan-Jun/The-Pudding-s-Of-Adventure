ee33d116f44b9f937fc1ae2cdb3031f6d7b62a1b
pudding <c1224412278@gmail.com>
2019-02-28 16:26:17 +0800

    update

---
+++ The-Pudding-s-Of-Adventure/Assets/Editor/EditorCreator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+public class EditorCreator : Editor
+{
+    [MenuItem("Customer/ScriptableObject/Create PlayerData")]
+    static void Fn_CreateScriptableObject()
+    {
+        if (!Directory.Exists("Resources"))
+            Directory.CreateDirectory("Assets/Resources");
+
+        string path =  "Assets/Resources/Player.asset";
+        PlayerData data = CreateInstance<PlayerData>();
+        AssetDatabase.CreateAsset(data , path);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+}
---
+++ The-Pudding-s-Of-Adventure/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public PlayerData m_thePlayerData { get; set; }
+
+    private Vector2 v2_Controller;
+    private Rigidbody2D m_rigidbody;
+    [SerializeField] private LayerMask Layer_Floor;
+    private void OnEnable()
+    {
+        m_thePlayerData = Resources.Load<PlayerData>("Player");
+    }
+    private void Start()
+    {
+        m_rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+    private void LateUpdate()
+    {
+        v2_Controller = new Vector2(Input.GetAxis("Horizontal") , Input.GetAxis("Vertical"));
+        if (v2_Controller.sqrMagnitude > 0f)
+        {
+            m_rigidbody.velocity = new Vector2(v2_Controller.x * m_thePlayerData.m_fMoveSpped, m_rigidbody.velocity.y);
+        }
+        else
+        {
+            m_rigidbody.velocity = new Vector2(0f , m_rigidbody.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))            //跳躍
+        {
+            if (Fn_IsInFloor())
+            {
+                m_rigidbody.AddForce(Vector2.up * m_thePlayerData.m_fJumpHigh);
+            }
+        }
+    }
+    private bool Fn_IsInFloor()                 //判斷是否在地面
+    {
+        Ray2D ray = new Ray2D(this.transform.position , Vector2.down);
+        return Physics2D.Raycast(ray.origin, ray.direction , 0.85f , Layer_Floor);
+    }
+}
+[System.Serializable]
+public class PlayerData : ScriptableObject
+{
+    public int m_iMaxHp;
+    public int m_iHp;
+    public float m_fMoveSpped;
+    public float m_fJumpHigh;
+}
